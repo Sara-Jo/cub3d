6c5350463e5f2e3eb2a8621cfa6bc8813045117c
hoseong511 <thdghtjd115@naver.com>
2022-12-07 04:08:23 +0900

    Update parsing : 공백라인을 고려하지 않은 데이터 로딩

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: hossong <hossong@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 19:55:15 by hossong           #+#    #+#             */
-/*   Updated: 2022/12/06 22:00:04 by hossong          ###   ########.fr       */
+/*   Updated: 2022/12/07 04:06:28 by hossong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,34 +38,64 @@ int	access_file(char *file)
 	return (fd);
 }
 
-void	file_to_data(int fd, t_data *data)
+char	**file_to_rawdata(int fd, int depth)
 {
-	char	*l;
+	char	**des;
+	char	*line;
 
-	(void) data;
-	l = get_next_line(fd);
-	while (l)
-	{
-		if (ft_strnstr(l, "NO", 2) && ft_isspace(*(l + 2)))
-			;
-		else if (ft_strnstr(l, "SO", 2) && ft_isspace(*(l + 2)))
-			printf("%s", l);
-		else if (ft_strnstr(l, "WE", 2) && ft_isspace(*(l + 2)))
-			printf("%s", l);
-		else if (ft_strnstr(l, "EA", 2) && ft_isspace(*(l + 2)))
-			printf("%s", l);
-		else if (ft_strnstr(l, "F", 1) && ft_isspace(*(l + 1)))
-			printf("%s", l);
-		else if (ft_strnstr(l, "C", 1) && ft_isspace(*(l + 1)))
-			printf("%s", l);
-		free(l);
-		l = get_next_line(fd);
-	}
+	des = 0;
+	line = get_next_line(fd);
+	if (line == NULL)
+		des = (char **)malloc(sizeof(char *) * depth);
+	else
+		des = file_to_rawdata(fd, depth + 1);
+	if (des)
+		des[depth] = line;
+	return (des);
 }
 
-int	validate_data(t_data *data)
+char	**load_map(char **raw, int depth)
 {
-	(void) data;
+	char	**map;
+
+	map = 0;
+	if (*raw == NULL)
+		map = (char **)malloc(sizeof(char *) * depth);
+	else
+		map = load_map(raw + 1, depth + 1);
+	if (map)
+		map[depth] = *raw;
+	return (map);
+}
+
+int	validate_data(char **raw, t_data *data)
+{
+	int		i;
+
+	i = 0;
+	while (raw[i] && i < 6)
+	{
+		if (ft_strnstr(raw[i], "NO", 2) && ft_isspace(*(raw[i] + 2)))
+			data->texture_no = raw[i];
+		else if (ft_strnstr(raw[i], "SO", 2) && ft_isspace(*(raw[i] + 2)))
+			data->texture_so = raw[i];
+		else if (ft_strnstr(raw[i], "WE", 2) && ft_isspace(*(raw[i] + 2)))
+			data->texture_we = raw[i];
+		else if (ft_strnstr(raw[i], "EA", 2) && ft_isspace(*(raw[i] + 2)))
+			data->texture_ea = raw[i];
+		else if (ft_strnstr(raw[i], "F", 1) && ft_isspace(*(raw[i] + 1)))
+			data->f_color = 1;
+		else if (ft_strnstr(raw[i], "C", 1) && ft_isspace(*(raw[i] + 1)))
+			data->c_color = 1;
+		i++;
+	}
+	data->map = load_map(&raw[i], 0);
+	while (*(data->map))
+	{
+		printf("%s", *(data->map));
+		(data->map)++;
+	}
+	free(raw);
 	return (0);
 }
 
@@ -73,14 +103,16 @@ int	main(int argc, char **argv)
 {
 	t_data	data;
 	int		fd;
+	char	**raw;
 
+	(void) data;
 	if (argc != 2)
 		return (1);
 	fd = access_file(argv[1]);
 	if (fd == -1)
 		return (1);
-	file_to_data(fd, &data);
-	if (validate_data(&data) == -1)
+	raw = file_to_rawdata(fd, 0);
+	if (validate_data(raw, &data) == -1)
 	{
 		ft_putstr_fd("validate_data error\n", 2);
 		return (1);
