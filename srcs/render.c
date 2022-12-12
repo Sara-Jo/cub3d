7b8830dc6d2fa3db80dfff1b9a91ebf9e2e3423b
hoseong511 <thdghtjd115@naver.com>
2022-12-12 16:08:50 +0900

    Update render : load_texture

--- srcs/render.c
+++ srcs/render.c
@@ -6,13 +6,14 @@
 /*   By: hossong <hossong@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 15:33:08 by hossong           #+#    #+#             */
-/*   Updated: 2022/12/12 01:21:03 by hossong          ###   ########.fr       */
+/*   Updated: 2022/12/12 15:48:27 by hossong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.h"
 #include "libft.h"
 #include "mlx.h"
+#include <stdio.h>
 
 void	my_mlx_pixel_put(t_img *data, int x, int y, int color)
 {
@@ -20,6 +21,36 @@ void	my_mlx_pixel_put(t_img *data, int x, int y, int color)
 
 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
 	*(unsigned int *)dst = color;
+}
+
+void	load_texture(t_data *a, char *path)
+{
+	t_img			img;
+	t_wh			size;
+	t_wh			idx;
+	unsigned int	*dst;
+
+	img.ptr = mlx_xpm_file_to_image(a->sc.mlx, path, &size.x, &size.y);
+	dst = (unsigned int *)malloc(size.x * size.y);
+	if (!img.addr || !dst)
+	{
+		perror(path);
+		exit(1);
+	}
+	img.addr = mlx_get_data_addr(img.ptr, &img.bits_per_pixel, \
+								&img.line_length, &img.endian);
+	idx.x = 0;
+	while (idx.x < size.x)
+	{
+		idx.y = 0;
+		while (idx.y < size.y)
+		{
+			idx.y++;
+		}
+		idx.x++;
+	}
+
+	mlx_destroy_image(a->sc.mlx, img.ptr);
 }
 
 void	make_texture(unsigned int texture[8][TEXHEIGHT * TEXWIDTH])
@@ -61,19 +92,20 @@ void	render(t_data *a)
 	make_texture(texture);
 	ft_bzero(a->img.addr, WIDTH * HEIGHT * a->img.bits_per_pixel / 8);
 	x = 0;
-	while (x < HEIGHT)
+	while (x < HEIGHT / 2)
 	{
 		y = 0;
 		while (y < WIDTH)
 		{
 			color = 0xffff00;
-			my_mlx_pixel_put(&a->img, y, x, color);
+			my_mlx_pixel_put(&a->img, y, HEIGHT / 2 + x, color);
 			color = 0xff00ff;
-			my_mlx_pixel_put(&a->img, y, HEIGHT - x - 1, color);
+			my_mlx_pixel_put(&a->img, y, HEIGHT / 2 - x - 1, color);
 			y++;
 		}
 		x++;
 	}
 	cast_wall(a);
+	load_texture(a, "./texture/wall.xpm");
 	mlx_put_image_to_window(a->sc.mlx, a->sc.mlx_win, a->img.ptr, 0, 0);
 }
